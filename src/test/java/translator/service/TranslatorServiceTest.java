e327a6d99475a830f9835134ad40f5707eb59505
Daniel <dforcen@hotmail.com>
2015-11-01 13:50:22 +0100

    test text changed and key added

--- src/test/java/translator/service/TranslatorServiceTest.java
+++ src/test/java/translator/service/TranslatorServiceTest.java
@@ -20,8 +20,8 @@ public class TranslatorServiceTest {
     TranslatorService translatorService;
     @Test
     public void translateTest() throws Exception {
-        TranslatedText translatedText = translatorService.translate("en", "es", "This is a test of translation service");
-        assertEquals("Esto es una prueba de servicio de traducción",translatedText.getTranslation());
+        TranslatedText translatedText = translatorService.translate("en", "es", "If you have a dream, go chase it. Don´t give up until you grab it");
+        assertEquals("Si tienes un sueño, ir a cazarlo. No te rindas hasta que lo agarra",translatedText.getTranslation());
     }
 
 }
--- src/test/java/translator/web/ws/TranslatorEnpointTest.java
+++ src/test/java/translator/web/ws/TranslatorEnpointTest.java
@@ -42,11 +42,11 @@ public class TranslatorEnpointTest {
 		GetTranslationRequest request = new GetTranslationRequest();
 		request.setLangFrom("en");
 		request.setLangTo("es");
-		request.setText("This is a test of translation service");
+		request.setText("If you have a dream, go chase it. Don´t give up until you grab it");
 		Object response = new WebServiceTemplate(marshaller).marshalSendAndReceive("http://localhost:"
 				+ port + "/ws", request);
 		assertNotNull(response);
 		assertThat(response, instanceOf(GetTranslationResponse.class));
-		assertThat(((GetTranslationResponse) response).getTranslation(), is("Esto es una prueba de servicio de traducción"));
+		assertThat(((GetTranslationResponse) response).getTranslation(), is("Si tienes un sueño, ir a cazarlo. No te rindas hasta que lo agarra"));
 	}	
 }
